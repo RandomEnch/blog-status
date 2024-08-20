5db33320addea05dad802ddc2696c1aefb724335
RandomEnch <1048421623@qq.com>
2024-08-20 17:56:30 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'RE 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2654581-0e4332204875beaf15a757ae',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/RandomEnch'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.randench.cn/'
+    },
+  ],
+};
